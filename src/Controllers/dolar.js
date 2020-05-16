de2e6cf4d99a55ec0bc7f7cb61e9c3b3277854b9
Ricardo Morato Rocha <ricardomorator@hotmail.com>
2020-05-16 16:18:39 -0300

    (develop): Added pokemon to the dolar controller.

--- src/Controllers/dolar.js
+++ src/Controllers/dolar.js
@@ -4,7 +4,11 @@ async function dolar(channel) {
   const dolarArray = await axios.get('https://economia.awesomeapi.com.br/json/daily/USD-BRL/15');
   const dolarToday = dolarArray.data[0].high;
 
-  channel.send(`Hoje o dólar está: ${dolarToday}`);
+  const pokemonNumber = `${(dolarToday * 100)}`.split('.');
+  const pokemonData = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonNumber[0]}`);
+  const pokemon = pokemonData.data.name;
+
+  channel.send(`Dólar hoje: ${dolarToday}, Pokemon: ${pokemon}`);
 }
 
 module.exports = { dolar };
